07685d9f05a1d37aae93931670f4a664cbeeb631
RadyAbdel <radyabdel1@gmail.com>
2023-03-07 15:52:57 +0100

    modif imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+
+import 'screens/login_screen/login_screen.dart';
+import 'utils/constants.dart';
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:login_screen/main.dart';
-import 'utils/constants.dart';
+
 
 
 void main() {
