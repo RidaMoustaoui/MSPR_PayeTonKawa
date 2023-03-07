25987d9d9acbc574d4cdff422af8ed1233f67003
RadyAbdel <radyabdel1@gmail.com>
2023-03-07 15:35:08 +0100

    Revert "modif imports"
    This reverts commit 65863977428b129ef2c0cd520df1a978b8cde5b7.

--- lib/screens/login_screen/components/bottom_text.dart
+++ lib/screens/login_screen/components/bottom_text.dart
@@ -1,27 +1,11 @@
-import 'utils/constants.dart';
-import 'dart:math' as math;
-import 'components/center_widget/center_widget.dart';
-import 'components/login_content.dart';
-import 'package:login_screen/screens/login_screen/animations/change_screen_animation.dart';
-import 'package:ionicons/ionicons.dart';
-import 'package:login_screen/utils/helper_functions.dart';
-import '../animations/change_screen_animation.dart';
-import 'bottom_text.dart';
-import 'top_text.dart';
 import 'package:flutter/material.dart';
+import 'package:login_screen/screens/login_screen/animations/change_screen_animation.dart';
+import 'package:login_screen/utils/helper_functions.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
-import 'package:login_screen/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-import '../components/login_content.dart';
-import 'dart:async';
-import 'package:flutter/widgets.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'dart:ui';
-import 'center_widget_clipper.dart';
-import 'center_widget_painter.dart';
 import '../../../utils/constants.dart';
 import 'login_content.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:login_screen/main.dart';
 
 class BottomText extends StatefulWidget {
   const BottomText({Key? key}) : super(key: key);
