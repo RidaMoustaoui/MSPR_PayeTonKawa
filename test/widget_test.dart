65863977428b129ef2c0cd520df1a978b8cde5b7
RadyAbdel <radyabdel1@gmail.com>
2023-03-07 15:33:13 +0100

    modif imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,30 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'utils/constants.dart';
+import 'dart:math' as math;
+import 'components/center_widget/center_widget.dart';
+import 'components/login_content.dart';
+import 'package:login_screen/screens/login_screen/animations/change_screen_animation.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:login_screen/utils/helper_functions.dart';
+import '../animations/change_screen_animation.dart';
+import 'bottom_text.dart';
+import 'top_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:encrypt/encrypt.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:login_screen/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../components/login_content.dart';
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:ui';
+import 'center_widget_clipper.dart';
+import 'center_widget_painter.dart';
+import '../../../utils/constants.dart';
+import 'login_content.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
