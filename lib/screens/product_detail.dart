7b7166d1be0c1d478ae9d334b5c6da4de42efc70
RadyAbdel <radyabdel1@gmail.com>
2023-03-27 22:28:04 +0200

    maj RA

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_screen/screens/augmented_reality.dart';
 import 'package:login_screen/screens/produits.dart';
 
 class ProduitDetail extends StatefulWidget {
@@ -13,17 +14,25 @@ class _ProduitDetailState extends State<ProduitDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           title: const Text('Détails du produit'),
         ),
-      body: Material(
-        borderRadius:
-            const BorderRadius.all(Radius.circular(20)),
-        color: Colors.grey.shade50,
-        elevation: 3,
-        child: ListTile(
-          title: Text("\nNom : \n\t"+widget.produit.name+"\n\n"+"Description : \n\t"+widget.produit.description+"\n"+"\nPrix : \n\t"+widget.produit.price+" Euros\n"+"\nCouleur : \n\t"+widget.produit.color+"\n"+"\nStock : \n\t"+widget.produit.stock.toString()+"\n")
-        ))
-    );
+        body: Material(
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          color: Colors.grey.shade50,
+          elevation: 3,
+          child: ListTile(
+              title: Text(
+                  "\nNom : \n\t${widget.produit.name}\n\nDescription : \n\t${widget.produit.description}\n\nPrix : \n\t${widget.produit.price} Euros\n\nCouleur : \n\t${widget.produit.color}\n\nStock : \n\t${widget.produit.stock}\n"))
+        ),
+        floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const AugmentedReality();
+        }));
+        },
+        child: const Icon(Icons.adb_rounded),
+      ),
+      );
   }
 }
